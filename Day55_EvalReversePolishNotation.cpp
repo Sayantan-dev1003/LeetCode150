c6cdb15816e487e0853ed6b12e09910d2fa206b6
Sayantan Halder <144941687+Sayantan-dev1003@users.noreply.github.com>
2025-04-04 20:53:50 +0530

    Create Day55_EvalReversePolishNotation.cpp

---
+++ Day55_EvalReversePolishNotation.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int evalRPN(vector<string>& tokens) {
+        stack<int> st;
+        for (string& token : tokens) {
+            if (token == "+" || token == "-" || token == "*" || token == "/") {
+                int b = st.top();
+                st.pop();
+                int a = st.top();
+                st.pop();
+                int res = 0;
+                if (token == "+")
+                    res = a + b;
+                else if (token == "-")
+                    res = a - b;
+                else if (token == "*")
+                    res = a * b;
+                else if (token == "/")
+                    res = a / b;
+                st.push(res);
+            } else {
+                st.push(stoi(token));
+            }
+        }
+        return st.top();
+    }
+};
