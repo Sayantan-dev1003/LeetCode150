5428e125c388e5251dde0b26e3243664e1b0e139
Sayantan Halder <144941687+Sayantan-dev1003@users.noreply.github.com>
2025-04-11 21:59:55 +0530

    Create Day62_ReverseNodesInKGroups.cpp

---
+++ Day62_ReverseNodesInKGroups.cpp
@@ -0,0 +1,42 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* reverseKGroup(ListNode* head, int k) {
+        if (head == nullptr || k == 1)
+            return head;
+
+        ListNode dummy(0);
+        dummy.next = head;
+        ListNode *curr = &dummy, *nex = &dummy, *prev = &dummy;
+        int count = 0;
+
+        while (curr->next != nullptr) {
+            curr = curr->next;
+            count++;
+        }
+
+        while (count >= k) {
+            curr = prev->next;
+            nex = curr->next;
+            for (int i = 1; i < k; i++) {
+                curr->next = nex->next;
+                nex->next = prev->next;
+                prev->next = nex;
+                nex = curr->next;
+            }
+            prev = curr;
+            count -= k;
+        }
+
+        return dummy.next;
+    }
+};
