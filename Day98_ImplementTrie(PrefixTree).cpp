eb6e64fccfdeca26c8c52f8d7b9c5354b6ae550b
Sayantan Halder <144941687+Sayantan-dev1003@users.noreply.github.com>
2025-05-17 14:00:19 +0530

    Create Day98_ImplementTrie(PrefixTree).cpp

---
+++ Day98_ImplementTrie(PrefixTree).cpp
@@ -0,0 +1,61 @@
+class TrieNode {
+public:
+    TrieNode* children[26];
+    bool isEndOfWord;
+
+    TrieNode() {
+        isEndOfWord = false;
+        for (auto& child : children)
+            child = nullptr;
+    }
+};
+
+class Trie {
+private:
+    TrieNode* root;
+
+public:
+    Trie() { root = new TrieNode(); }
+
+    void insert(string word) {
+        TrieNode* node = root;
+        for (char ch : word) {
+            int idx = ch - 'a';
+            if (!node->children[idx]) {
+                node->children[idx] = new TrieNode();
+            }
+            node = node->children[idx];
+        }
+        node->isEndOfWord = true;
+    }
+
+    bool search(string word) {
+        TrieNode* node = root;
+        for (char ch : word) {
+            int idx = ch - 'a';
+            if (!node->children[idx])
+                return false;
+            node = node->children[idx];
+        }
+        return node->isEndOfWord;
+    }
+
+    bool startsWith(string prefix) {
+        TrieNode* node = root;
+        for (char ch : prefix) {
+            int idx = ch - 'a';
+            if (!node->children[idx])
+                return false;
+            node = node->children[idx];
+        }
+        return true;
+    }
+};
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie* obj = new Trie();
+ * obj->insert(word);
+ * bool param_2 = obj->search(word);
+ * bool param_3 = obj->startsWith(prefix);
+ */
